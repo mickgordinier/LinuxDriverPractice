68ea485bfe6b56ca7385181173fad85a588afdbe
Mick Gordinier <mickgordinier@gmail.com>
2025-05-20 09:03:12 +0100

    Created working python user app to write/read from proc file

--- user-app.py
+++ user-app.py
@@ -1,5 +1,9 @@
 def main():
-    driver_handle = open('/proc/mick_driver')
+    # Seeking not available for proc files, must open for read-only and write-only
+    driver_handle = open('/proc/mick_driver', 'w')
+    driver_handle.write("Hello, World!!");
+    driver_handle.close();
+    driver_handle = open('/proc/mick_driver', 'r')
     message_from_kernel_space = driver_handle.readline()
     print(message_from_kernel_space)
     driver_handle.close();

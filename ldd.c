4b1e9782f447a5523a01b4f7438a4b3c875e07d6
Mick Gordinier <mickgordinier@gmail.com>
2025-05-20 08:39:46 +0100

    Working proc write to driver buffer

--- ldd.c
+++ ldd.c
@@ -12,16 +12,19 @@ MODULE_DESCRIPTION("First dynamically loadable kernel module");
 // The virtual proc file entry node
 static struct proc_dir_entry *proc_entry;
 
-// Creating read function to handle file read system call
+static char kernel_buffer[1000];
+static size_t kbuf_offset = 0;
+
+// Creating read function to handle proc file read system call
 // Parameters filled in by kernel
 // NOTE: FUNCTION IS REPEATEDLY CALLED UNTIL 0 IS RETURNED
-static ssize_t 
+static ssize_t
 mick_read(
 	struct file* file,       // (ignored) Open proc file being read
 	char *user_space_buffer, // Where read data should be copies to (Fresh every call)
-	size_t count,            // (ignored) Specifies how many bytes user wants to read
+	size_t count,            // Specifies how many bytes user wants to read
 	loff_t* offset           // Internal offset value to keep track of msg idx
-) 
+)
 {
 	// For kernel buffer debug
 	printk("mick_read\n");
@@ -30,7 +33,7 @@ mick_read(
 	char msg[] = "Ack!\n";
 	size_t len = strlen(msg);
 
-	size_t bytesToRead = min(len - *offset, 1);
+	size_t bytesToRead = min(len - *offset, count);
 
 	// Offset is initialized to 0 during cat calls
 	// Therefore, we want to copy over the entirity of msg by keeping track of offset
@@ -42,8 +45,8 @@ mick_read(
 	// copy call is synchronous and will wait until all bytes are copied
 	if (copy_to_user(
 		user_space_buffer,  // buffer pointer is corrected each read() call
-		msg + *offset, 
-		bytesToRead) != 0) 
+		msg + *offset,
+		bytesToRead) != 0)
 	{
 		// returns # of bytes not copied
 		return -EFAULT;
@@ -56,20 +59,48 @@ mick_read(
 	return bytesToRead;
 }
 
+// Creating read function to handle proc file write system call
+// Parameters filled in by kernel
+static ssize_t
+mick_write(
+	struct file* file,       // (ignored) Open proc file being written to
+	const char *user_space_buffer, // Data to copy to kernel space
+	size_t count,            // Specifies how many bytes user wants to write
+	loff_t* offset           // Internal offset value to keep track of msg idx
+)
+{
+	printk("mick_write\n");
+
+	copy_from_user(
+		kernel_buffer + kbuf_offset,
+		user_space_buffer,
+		count);
+
+	kbuf_offset += count;
+
+	printk("write complete\n");
+	printk("Buffer: %s\n", kernel_buffer);
+	printk("First 2 Chars: %x %x\n", kernel_buffer[0], kernel_buffer[1]);
+	printk("Buffer Offset: %d\n", kbuf_offset);
+
+	return count;
+}
+
 // Allow proc operation to use my custom function during syscalls
 struct proc_ops driver_proc_ops = {
-	.proc_read = mick_read
+	.proc_read = mick_read,
+	.proc_write = mick_write
 };
 
-static int 
+static int
 mick_module_init (void) {
   printk("mick_module_init: entry\n");
 
   // In /proc/, creating virtual file mick_driver
   // When kernel loads module (insmod), calls my module_init function
-  // Should now exist virtual file /proc/mick_driver	
-  proc_entry = proc_create("mick_driver", 0, NULL, &driver_proc_ops); 
-  
+  // Should now exist virtual file /proc/mick_driver
+  proc_entry = proc_create("mick_driver", 0666, NULL, &driver_proc_ops);
+
   printk("mick_module_init: exit\n");
   return 0;
 }
